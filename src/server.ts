cce65bb961e2792cadd0ddd4e2605c59584ea084
SdZika <markozivkovic1982@gmail.com>
2025-02-03 21:50:07 +0100

    update tsconfig.json

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
-const express = require('express');
-
-
-const dotenv = require('dotenv');
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 9000;
+const app: Express = express();
+const port = process.env.PORT || 9000;
 
-app.get('/', (req:any, res:any) => {
-  res.send('Hello World MimiKa Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
